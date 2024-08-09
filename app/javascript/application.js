64b170eaa0e6f7960bf6fcff45254b64d1a8bac4
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2024-08-08 22:54:48 -0400

    menu and profile open/close

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.querySelector('[aria-controls="mobile-menu"]');
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const profileButton = document.getElementById("user-menu-button");
+  const userMenu = document.getElementById("user-menu");
+
+  menuButton.addEventListener("click", function () {
+    const isExpanded = menuButton.getAttribute("aria-expanded") === "true";
+    menuButton.setAttribute("aria-expanded", !isExpanded);
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  profileButton.addEventListener("click", function () {
+    userMenu.classList.toggle("hidden");
+  });
+});
